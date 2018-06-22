8b698ebff06356f30c8bdbbd24317f7c9c663e17
zhouhouhong <11102130@163.com>
2018-06-22 14:43:03 +0800

    add testcode

---
+++ platforms/android/app/src/main/java/io/ionic/starter/AppActivity.java
@@ -0,0 +1,117 @@
+/*
+       Licensed to the Apache Software Foundation (ASF) under one
+       or more contributor license agreements.  See the NOTICE file
+       distributed with this work for additional information
+       regarding copyright ownership.  The ASF licenses this file
+       to you under the Apache License, Version 2.0 (the
+       "License"); you may not use this file except in compliance
+       with the License.  You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+       Unless required by applicable law or agreed to in writing,
+       software distributed under the License is distributed on an
+       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+       KIND, either express or implied.  See the License for the
+       specific language governing permissions and limitations
+       under the License.
+ */
+
+package io.ionic.starter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.cordova.CordovaActivity;
+
+import cn.jpush.android.api.JPushInterface;
+
+public class AppActivity extends CordovaActivity
+{
+
+  public String TAG = "Elastos";
+  static {
+//    System.loadLibrary("spvsdk");
+//    System.loadLibrary("elastoswallet");
+
+  }
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+
+        // Set by <content src="index.html" /> in config.xml
+        //loadUrl(launchUrl);
+        Log.e(TAG, launchUrl);
+
+      String scheme = "";
+      String host = "";
+      String startParams= "";
+      Intent intent = getIntent();
+      Log.e(TAG, "intent: " + intent);
+      if (null != intent) {
+        Uri data = intent.getData();
+        Log.e(TAG, "data: " + data);
+        if (null != data) {
+          scheme = data.getScheme();
+          host = data.getHost();
+          if (scheme.equals("elastos") && host.equals("elastos")) {
+            //String path = data.getPath();
+            //startParams = path.substring(1) + ";mm";
+            loadUrl("file:///android_asset/samples/www/index.html");
+          } else {
+            startParams = data.getQuery();
+          }
+        } else {
+          Log.e(TAG,"data is null");
+          String param = intent.getStringExtra("param");
+//          if ("".param.isEmpty(param)) {
+//            int index = param.indexOf("?");
+//            param = param.substring(index + 1);
+//          }
+          //startParams = (param != null ? param : "");
+          loadUrl("file:///android_asset/www/index.html");
+        }
+      } else {
+        loadUrl("file:///android_asset/www/index.html");
+      }
+
+        //initJG();
+    }
+
+
+    private void initJG(){
+        MyUtil.moveConfigFiles2RootPath(this);
+
+        Context applicationContext = getApplicationContext();
+        MyUtil.setApplicationContext(applicationContext);
+
+        String udid =  MyUtil.getImei(applicationContext, "");
+        if (null != udid) Log.w("xxl-jg","Imei uuid is " + udid);
+
+        String appKey = MyUtil.getAppKey(applicationContext);
+        if (null == appKey) appKey = "AppKey异常";
+        Log.w("xxl-jg","AppKey " + appKey);
+
+        String packageName =  getPackageName();
+        Log.w("xxl-jg","PackageName " + packageName);
+
+        String deviceId = MyUtil.getDeviceId(applicationContext);
+        Log.w("xxl-jg","deviceId " + deviceId);
+
+        String versionName =  MyUtil.GetVersion(applicationContext);
+        Log.w("xxl-jg","versionName " + versionName);
+
+        //
+        JPushInterface.init(applicationContext);
+    }
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { ModalController} from 'ionic-angular';
+
+import { AddItemPage } from '../add-item/add-item';
+
+//import { IonicStorageModule } from '@ionic/storage';
+import { DataProvider } from '../../providers/data/data';
+
+
+//import Wallet;
+declare let cordova: any;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ public items;
+  constructor(public modalCtrl: ModalController, public dataService: DataProvider) {
+
+	this.dataService.getData().then((todos) => { 
+      if(todos){
+        this.items = JSON.parse(todos); 
+      } else {
+		this.items = [
+		  {title: 'Elastos TODO1', description: 'TODO List1'},
+		  {title: 'Elastos TODO2', description: 'TODO List2'},
+		  {title: 'Elastos TODO3', description: 'TODO List3'}
+		];
+	  }
+    });
+
+  } 
+  ionViewDidLoad(){
+
+    /* 开始，
+	  	//Second  Carrier  Wallet  UID components 
+	var a_wallet = new Wallet()
+    result = a_wallet.CreateSubWallet (
+             chainID,  
+           coinTypeIndex,    // 新增，币种的index号，用于构建路径
+           payPassword,
+          singleAddress)
+		
+		  
+     this.items = [
+      {title: 'Elastos TODO1', description: 'TODO List1'},
+      {title: 'Elastos TODO2', description: 'TODO List2'},
+      {title: 'Elastos TODO3', description: 'TODO List3'}
+    ];
+	   */
+   }
+   
+	addItem(){
+     let addModal = this.modalCtrl.create(AddItemPage);
+     addModal.onDidDismiss((item) => {
+           if(item){
+            this.saveItem(item);
+          }
+     });
+     addModal.present();
+   }
+   
+   saveItem(item){
+    this.items.push(item);
+    this.dataService.save(this.items);
+   }
+
+   viewItem(){
+	   //cordova.plugins.AppManager.run();
+	   cordova.plugins.TestPlugin.coolMethod("今天好运气，一老狼请吃鸡呀！",result=>alert(result),error=>alert(error));
+   }
+
+}
